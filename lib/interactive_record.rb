2f056e3b87984feec2cf0fcb18ac97153945bb07
Arif Cagirici <acagirici23@gmail.com>
2020-10-25 02:28:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = 
     
-    column_names = DB[:conn].execute
+    column_names = DB[:conn].execute("PRAGMA")
   end
   
 end
